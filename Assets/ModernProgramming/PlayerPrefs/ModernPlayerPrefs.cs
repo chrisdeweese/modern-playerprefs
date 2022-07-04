10ab996694a072d2454348e18b481f829f1aff3b
Chris D <chris@deweese.com>
2022-07-04 01:03:05 -0500

    Added support for save/load Vector2

--- Assets/ModernProgramming/PlayerPrefs/ModernPlayerPrefs.cs
+++ Assets/ModernProgramming/PlayerPrefs/ModernPlayerPrefs.cs
@@ -6,9 +6,25 @@ public class PlayerPrefsExtended : MonoBehaviour
     {
         PlayerPrefs.SetInt(valueName, value);
     }
+
+    public void SetVector2(string valueName, Vector2 value)
+    {
+        string result = value.x + "," + value.y;
+        PlayerPrefs.SetString(valueName, result);
+    }
     
     public int GetInt(string valueName, int defaultValue)
     {
         return PlayerPrefs.GetInt(valueName, defaultValue);
+    }
+
+    public Vector2 GetVector2(string valueName, Vector2 defaultValue)
+    {
+        string defaultString = defaultValue.x + "," + defaultValue.y;
+        
+        string value = PlayerPrefs.GetString(valueName, defaultString);
+        string[] components = value.Split(',');
+
+        return new Vector2(float.Parse(components[0]), float.Parse(components[1]));
     }
 }
