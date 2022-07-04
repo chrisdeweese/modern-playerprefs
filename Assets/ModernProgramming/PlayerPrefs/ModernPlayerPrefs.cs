025c2c084871d5295a2f1d411275f079c770006b
Chris D <chris@deweese.com>
2022-07-04 01:04:34 -0500

    Added support for save/load Vector4

--- Assets/ModernProgramming/PlayerPrefs/ModernPlayerPrefs.cs
+++ Assets/ModernProgramming/PlayerPrefs/ModernPlayerPrefs.cs
@@ -1,46 +1,65 @@
 using UnityEngine;
 
-public class PlayerPrefsExtended : MonoBehaviour
+namespace ModernProgramming
 {
-    public void SetInt(string valueName, int value)
+    public class PlayerPrefsExtended : MonoBehaviour
     {
-        PlayerPrefs.SetInt(valueName, value);
-    }
+        public void SetInt(string valueName, int value)
+        {
+            PlayerPrefs.SetInt(valueName, value);
+        }
 
-    public void SetVector2(string valueName, Vector2 value)
-    {
-        string result = value.x + "," + value.y;
-        PlayerPrefs.SetString(valueName, result);
-    }
+        public void SetVector2(string valueName, Vector2 value)
+        {
+            string result = value.x + "," + value.y;
+            PlayerPrefs.SetString(valueName, result);
+        }
 
-    public void SetVector3(string valueName, Vector3 value)
-    {
-        string result = value.x + "," + value.y + "," + value.z;
-        PlayerPrefs.SetString(valueName, result);
-    }
-    
-    public int GetInt(string valueName, int defaultValue)
-    {
-        return PlayerPrefs.GetInt(valueName, defaultValue);
-    }
+        public void SetVector3(string valueName, Vector3 value)
+        {
+            string result = value.x + "," + value.y + "," + value.z;
+            PlayerPrefs.SetString(valueName, result);
+        }
 
-    public Vector2 GetVector2(string valueName, Vector2 defaultValue)
-    {
-        string defaultString = defaultValue.x + "," + defaultValue.y;
+        public void SetVector4(string valueName, Vector4 value)
+        {
+            string result = value.w + "," + value.x + "," + value.y + "," + value.z;
+            PlayerPrefs.SetString(valueName, result);
+        }
         
-        string value = PlayerPrefs.GetString(valueName, defaultString);
-        string[] components = value.Split(',');
+        public int GetInt(string valueName, int defaultValue)
+        {
+            return PlayerPrefs.GetInt(valueName, defaultValue);
+        }
 
-        return new Vector2(float.Parse(components[0]), float.Parse(components[1]));
-    }
-    
-    public Vector2 GetVector3(string valueName, Vector3 defaultValue)
-    {
-        string defaultString = defaultValue.x + "," + defaultValue.y + "," + defaultValue.z;
+        public Vector2 GetVector2(string valueName, Vector2 defaultValue)
+        {
+            string defaultString = defaultValue.x + "," + defaultValue.y;
+            
+            string value = PlayerPrefs.GetString(valueName, defaultString);
+            string[] components = value.Split(',');
+
+            return new Vector2(float.Parse(components[0]), float.Parse(components[1]));
+        }
         
-        string value = PlayerPrefs.GetString(valueName, defaultString);
-        string[] components = value.Split(',');
+        public Vector2 GetVector3(string valueName, Vector3 defaultValue)
+        {
+            string defaultString = defaultValue.x + "," + defaultValue.y + "," + defaultValue.z;
+            
+            string value = PlayerPrefs.GetString(valueName, defaultString);
+            string[] components = value.Split(',');
 
-        return new Vector3(float.Parse(components[0]), float.Parse(components[1]), float.Parse(components[2]));
-    }
+            return new Vector3(float.Parse(components[0]), float.Parse(components[1]), float.Parse(components[2]));
+        }
+        
+        public Vector2 GetVector4(string valueName, Vector4 defaultValue)
+        {
+            string defaultString = defaultValue.x + "," + defaultValue.y + "," + defaultValue.z + "," + defaultValue.w;
+            
+            string value = PlayerPrefs.GetString(valueName, defaultString);
+            string[] components = value.Split(',');
+
+            return new Vector4(float.Parse(components[0]), float.Parse(components[1]), float.Parse(components[2]), float.Parse(components[3]));
+        }
+    }   
 }
