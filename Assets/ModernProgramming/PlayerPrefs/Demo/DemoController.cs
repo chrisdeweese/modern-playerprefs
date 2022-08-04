fd045ca2c72f54f974a3fd028f9f6d166ff58b8b
Chris D <chris@deweese.com>
2022-08-04 18:05:31 -0500

    Update DemoController.cs

--- Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
+++ Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,25 +8,49 @@ namespace ModernProgramming
 {
     public class DemoController : MonoBehaviour
     {
+        [SerializeField] private Dropdown typeDropdown;
         [SerializeField] private Text valueLabel;
         [SerializeField] private InputField inputFieldName;
-        [SerializeField] private InputField inputFieldSet;
-        [SerializeField] private Dropdown typeDropdown;
+        [SerializeField] private GameObject stringInputRow;
+        [SerializeField] private GameObject intInputRow;
+        [SerializeField] private GameObject floatInputRow;
+        [SerializeField] private GameObject boolInputRow;
+
+        private void Start()
+        {
+            typeDropdown.onValueChanged.AddListener(delegate
+            {
+                TypeDropdownChanged(typeDropdown);
+            });
+        }
+
+        private void TypeDropdownChanged(Dropdown change)
+        {
+            if (change.value == 0) //string
+            {
+                //stringInputRow
+            }
+        }
+
+        private void ResetInputRows()
+        {
+            
+        }
 
         //TODO: Auto-save
         public void onClick_Set()
         {
             if (typeDropdown.options[typeDropdown.value].text.ToLower() == "string")
             {
-                PlayerPrefs.SetString(inputFieldName.text, inputFieldSet.text.ToString());
+                //PlayerPrefs.SetString(inputFieldName.text, inputFieldSet.text.ToString());
             }
             else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "int")
             {
-                PlayerPrefs.SetInt(inputFieldName.text, int.Parse(inputFieldSet.text));
+                //PlayerPrefs.SetInt(inputFieldName.text, int.Parse(inputFieldSet.text));
             }
             else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "float")
             {
-                PlayerPrefs.SetFloat(inputFieldName.text, float.Parse(inputFieldSet.text));
+                //PlayerPrefs.SetFloat(inputFieldName.text, float.Parse(inputFieldSet.text));
             }
             
             Debug.Log("Modern PlayerPrefs Extended - Saved " + inputFieldName.text + " as " + typeDropdown.options[typeDropdown.value].text.ToLower());
