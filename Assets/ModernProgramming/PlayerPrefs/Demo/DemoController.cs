c5355e2b7f2da2debb3e9aa8fdb772c8691c9b82
Chris <chris@deweese.com>
2022-08-04 00:24:19 -0500

    Update DemoController.cs

--- Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
+++ Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
@@ -9,27 +9,55 @@ namespace ModernProgramming
     public class DemoController : MonoBehaviour
     {
         [SerializeField] private Text valueLabel;
-        [SerializeField] private InputField inputField;
+        [SerializeField] private InputField inputFieldName;
+        [SerializeField] private InputField inputFieldSet;
         [SerializeField] private Dropdown typeDropdown;
 
+        //TODO: Auto-save
         public void onClick_Set()
         {
+            if (typeDropdown.options[typeDropdown.value].text.ToLower() == "string")
+            {
+                PlayerPrefs.SetString(inputFieldName.text, inputFieldSet.text.ToString());
+            }
+            else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "int")
+            {
+                PlayerPrefs.SetInt(inputFieldName.text, int.Parse(inputFieldSet.text));
+            }
+            else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "float")
+            {
+                PlayerPrefs.SetFloat(inputFieldName.text, float.Parse(inputFieldSet.text));
+            }
             
+            Debug.Log("Modern PlayerPrefs Extended - Saved " + inputFieldName.text + " as " + typeDropdown.options[typeDropdown.value].text.ToLower());
         }
 
         public void onClick_Get()
         {
-            
+            if (typeDropdown.options[typeDropdown.value].text.ToLower() == "string")
+            {
+                valueLabel.text = PlayerPrefs.GetString(inputFieldName.text, "");
+            }
+            else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "int")
+            {
+                valueLabel.text = PlayerPrefs.GetInt(inputFieldName.text, 0).ToString();
+            }
+            else if (typeDropdown.options[typeDropdown.value].text.ToLower() == "float")
+            {
+                valueLabel.text = PlayerPrefs.GetFloat(inputFieldName.text, 0.0f).ToString();
+            }
         }
 
         public void onClick_Delete()
         {
-            
+            PlayerPrefs.DeleteKey(inputFieldName.text);
+            PlayerPrefs.Save();
         }
 
         public void onClick_DeleteAll()
         {
-            
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
         }
     }
 }
