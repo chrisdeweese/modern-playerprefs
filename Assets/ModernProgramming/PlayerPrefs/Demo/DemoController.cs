3dd9ff0e840aa8a850aabd71130122319766b59c
Chris <chris@deweese.com>
2022-08-05 00:07:11 -0500

    Added Vector2, Vector3 to demo

--- Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
+++ Assets/ModernProgramming/PlayerPrefs/Demo/DemoController.cs
@@ -15,6 +15,8 @@ namespace ModernProgramming
         [SerializeField] private GameObject intInputRow;
         [SerializeField] private GameObject floatInputRow;
         [SerializeField] private GameObject boolInputRow;
+        [SerializeField] private GameObject vector2InputRow;
+        [SerializeField] private GameObject vector3InputRow;
 
         private void Start()
         {
@@ -35,6 +37,8 @@ namespace ModernProgramming
             intInputRow.SetActive(change.value == 1);
             floatInputRow.SetActive(change.value == 2);
             boolInputRow.SetActive(change.value == 3);
+            vector2InputRow.SetActive(change.value == 4);
+            vector3InputRow.SetActive(change.value == 5);
         }
 
         private void ResetInputRows()
@@ -43,6 +47,8 @@ namespace ModernProgramming
             intInputRow.SetActive(false);
             floatInputRow.SetActive(false);
             boolInputRow.SetActive(false);
+            vector2InputRow.SetActive(false);
+            vector3InputRow.SetActive(false);
         }
 
         //TODO: Auto-save
@@ -68,40 +74,73 @@ namespace ModernProgramming
                 bool value = boolInputRow.GetComponentInChildren<Toggle>().isOn;
                 PlayerPrefsExtended.SetBool(keyNameInput.text, value);
             }
+            else if (typeDropdown.value == 4)
+            {
+                InputField[] inputs = vector2InputRow.GetComponentsInChildren<InputField>();
+                Vector2 value = new Vector2(float.Parse(inputs[0].text), float.Parse(inputs[1].text));
+                PlayerPrefsExtended.SetVector2(keyNameInput.text, value);
+            }
+            else if (typeDropdown.value == 5)
+            {
+                InputField[] inputs = vector3InputRow.GetComponentsInChildren<InputField>();
+                Vector3 value = new Vector3(float.Parse(inputs[0].text), float.Parse(inputs[1].text), float.Parse(inputs[2].text));
+                PlayerPrefsExtended.SetVector3(keyNameInput.text, value);
+            }
             
-            Debug.Log("Modern PlayerPrefs Extended - Saved " + keyNameInput.text + " as " + typeDropdown.options[typeDropdown.value].text.ToLower());
+            Debug.Log("PlayerPrefs Extended - Saved " + keyNameInput.text + " as " + typeDropdown.options[typeDropdown.value].text.ToLower());
         }
 
         public void onClick_Get()
         {
+            string result = "";
             if (typeDropdown.value == 0)
             {
-                valueLabel.text = PlayerPrefsExtended.GetString(keyNameInput.text, "");
+                result = PlayerPrefsExtended.GetString(keyNameInput.text, "");
+                valueLabel.text = result;
             }
             else if (typeDropdown.value == 1)
             {
-                valueLabel.text = PlayerPrefsExtended.GetInt(keyNameInput.text, 0).ToString();
+                result = PlayerPrefsExtended.GetInt(keyNameInput.text, 0).ToString();
+                valueLabel.text = result;
             }
-            if (typeDropdown.value == 2)
+            else if (typeDropdown.value == 2)
             {
-                valueLabel.text = PlayerPrefsExtended.GetFloat(keyNameInput.text, 0.0f).ToString();
+                result = PlayerPrefsExtended.GetFloat(keyNameInput.text, 0.0f).ToString();
+                valueLabel.text = result;
             }
-            if (typeDropdown.value == 3)
+            else if (typeDropdown.value == 3)
             {
-                valueLabel.text = PlayerPrefsExtended.GetBool(keyNameInput.text, false).ToString();
+                result = PlayerPrefsExtended.GetBool(keyNameInput.text, false).ToString();
+                valueLabel.text = result;
             }
+            else if (typeDropdown.value == 4)
+            {
+                result = PlayerPrefsExtended.GetVector2(keyNameInput.text, Vector2.zero).ToString();
+                valueLabel.text = result;
+            }
+            else if (typeDropdown.value == 5)
+            {
+                result = PlayerPrefsExtended.GetVector3(keyNameInput.text, Vector3.zero).ToString();
+                valueLabel.text = result;
+            }
+            
+            Debug.Log("PlayerPrefs Extended - Loaded " + keyNameInput.text + ", value was " + result);
         }
 
         public void onClick_Delete()
         {
             PlayerPrefs.DeleteKey(keyNameInput.text);
             PlayerPrefs.Save();
+
+            Debug.Log("PlayerPrefs Extended - Deleted Key: " + keyNameInput.text);
         }
 
         public void onClick_DeleteAll()
         {
             PlayerPrefs.DeleteAll();
             PlayerPrefs.Save();
+            
+            Debug.Log("PlayerPrefs Extended - Deleted All Keys!!!");
         }
     }
 }
